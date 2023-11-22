a6b83aa000dabba60d5ee7c08516e86cceeb6eec
khushi <khushibansal@gmail.com>
2023-11-22 15:53:24 +0530

    nodejs

--- index.js
+++ index.js
@@ -114,12 +114,125 @@
 // c=a+b;
 // console.log("addition is:"+c)
 
-//conectivity with server with nodemon (http)
-const http=require("http");
-const server =http.createServer((req,res)=>
-{
-    res.end("hello ssi digital indore");
-});
-server.listen(8000,"127.0.0.1",()=>{
-    console.log("i am 8000 listening")
-});
+//conectivity with server with nodemon (http) [//localhost:8000]
+// const http=require("http");
+// const server =http.createServer((req,res)=>
+// {
+//     res.end("hello ssi digital indore");
+// });
+// server.listen(8000,"127.0.0.1",()=>{
+//     console.log("i am 8000 listening")
+// });
+
+// //url 
+// const http=require("http");
+// const server =http.createServer((req,res)=>
+// {
+//     console.log(req.url);
+//     res.end("hello ");
+// });
+// server.listen(8000,"127.0.0.1",()=>{
+//     console.log("i am 8000 listening")
+// });
+
+////url different pages
+// const http=require("http");
+// const server =http.createServer((req,res)=>
+// {   
+//     if(req.url=="/")
+//     {
+//         res.end("i am home page")
+//     }
+//     else if(req.url=="/about")
+//     {
+//         res.end("i am about page")
+//     }
+//     else{
+//         res.end("error")
+//     }
+// });
+// server.listen(8000,"127.0.0.1",()=>{
+//     console.log("i am 8000 listening")
+// });
+
+// //factorial , table [url]
+// var http=require("http");
+// var server =http.createServer((req,res)=>
+// {   
+//     if(req.url=="/")
+//     {
+//         res.end("i am home page")
+//     }
+//     else if(req.url=="/fact")
+//     {   
+//         let f=1;
+//         for(i=1;i<=5;i++)
+//         {
+//             f=f*i;
+//         }
+//         res.end("fact is="+f)
+//     }
+
+//     else if(req.url=="/table")
+//     {   
+//         let t="";
+//         for(i=1;i<=10;i++)
+//         {
+//             t+=4*i+"\n";
+//         }
+//         res.end(t)
+//     }
+//     else{
+//         res.end("error")
+//     }
+// });
+// server.listen(8000,"127.0.0.1",()=>{
+//     console.log("i am 8000 listening")
+// });
+
+// //convert record in json [1) add in new file]
+// const fs=require("fs");
+// const a={name:"khushi", age:"20", city:"indore"};
+// const data =JSON.stringify(a);
+// fs.writeFile("json1.json" , data,(err)=>{
+//     console.log("done")
+// });
+
+// //[2) read file]
+// const fs=require("fs");
+// const a={name:"khushi", age:"20", city:"indore"};
+// const data =JSON.stringify(a);
+// fs.readFile("json1.json","utf-8", (err,data)=>{
+//     console.log(data);
+// });
+
+// //[3)again covert back to js object] (parse)
+// const fs=require("fs");
+// const a={name:"khushi", age:"20", city:"indore"};
+// fs.readFile("json1.json","utf-8", (err,data)=>{
+//     console.log(data);
+//     const org=JSON.parse(data);
+//     console.log(org)
+// });
+
+
+// //Events modules
+// //EventEmitter = is a class.
+// //emit= emit is used to trigger. 
+// //on= on is used to add a callback function that,s going to be executed  when the event is trigger.
+// const EventEmitter=require("events");
+// const event= new EventEmitter();
+// event.on("hello",()=>{
+//     console.log("khushi bansal.");
+// });
+// event.emit("hello");
+
+
+//streams in nodejs
+//-> streams are object thet let you read data from a source or write data to a destination in a continuous fashion. 
+//-> streamingmeans listening music or watching video in "real time" , insted of downloading a file to youe computer.
+// 4 types of streams
+//1) readable= stream which is used for read operation.
+//2) writable= use to write
+//3) duplex= used for both read and write
+//4) transform=
